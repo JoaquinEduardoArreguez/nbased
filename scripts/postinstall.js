9901332803dff9a0d0db99679fd90a71727bcd24
Alan Berdinelli <aeberdinelli@gmail.com>
2021-01-26 15:28:57 -0300

    Fix for CI post install output

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,25 +1,34 @@
 const styles = require('chalk');
 
-console.log('/' + new Array(process.stdout.columns - 2).fill('=').join('') + '\\');
+(function() {
+    // Fix for CI
+    if (typeof process.stdout.columns === 'undefined') {
+        console.log(styles.greenBright('✔ Successfully installed nbased'));
 
-console.log([
-    writeLine(''),
-    writeLine('✔ Successfully installed nbased', styles.greenBright),
-    writeLine(''),
-    writeLine('If you need help, visit https://github.com/aeberdinelli/nbased/wiki'),
-    writeLine('You can also check our new CLI by running:', styles.bold),
-    writeLine(''),
-    writeLine('npm install -g nbased-tools', styles.underline),
-    writeLine(''),
-].join('\n'));
-
-function writeLine(text, styling = null) {
-    if (styling) {
-        return '|  ' + styling(text) + new Array(process.stdout.columns - text.length - 6).fill(' ').join('') + '  |';
+        return;
     }
-
-    return '|  ' + text + new Array(process.stdout.columns - text.length - 6).fill(' ').join('') + '  |';
-}
-
-console.log('\\' + new Array(process.stdout.columns - 2).fill('=').join('') + '/');
-console.log('');
+    
+    console.log('/' + new Array(process.stdout.columns - 2).fill('=').join('') + '\\');
+    
+    console.log([
+        writeLine(''),
+        writeLine('✔ Successfully installed nbased', styles.greenBright),
+        writeLine(''),
+        writeLine('If you need help, visit https://github.com/aeberdinelli/nbased/wiki'),
+        writeLine('You can also check our new CLI by running:', styles.bold),
+        writeLine(''),
+        writeLine('npm install -g nbased-tools', styles.underline),
+        writeLine(''),
+    ].join('\n'));
+    
+    function writeLine(text, styling = null) {
+        if (styling) {
+            return '|  ' + styling(text) + new Array(process.stdout.columns - text.length - 6).fill(' ').join('') + '  |';
+        }
+    
+        return '|  ' + text + new Array(process.stdout.columns - text.length - 6).fill(' ').join('') + '  |';
+    }
+    
+    console.log('\\' + new Array(process.stdout.columns - 2).fill('=').join('') + '/');
+    console.log('');
+})();
